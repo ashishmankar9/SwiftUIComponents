7898dcf12e1c796e1603addb77332ca657455add
Ashish Mankar <ashishmankar9@gmail.com>
2021-12-12 13:46:16 +0530

    Added height related changes.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,17 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            BottomSheetView(transperancy: 0.2,cornerRadius: 40)
+            BottomSheetView(transperancy: 0.2,cornerRadius: 40) {}
         }
     }
 }
 
 struct AddYourComponents: View {
+    var action:()->()
     var body: some View {
         VStack{
             Text("Hello").bold().font(.title).padding()
-            Text("We are glat to find here.").bold().font(.subheadline).padding()
-            Button(action: {}, label: {
+            Text("We are glat to find you here.").bold().font(.subheadline).padding()
+            Button(action: {
+                action()
+                debugPrint("Button Tapped")
+            }, label: {
                 Text("Awesome")
             }).padding()
         }
@@ -30,27 +34,30 @@ struct AddYourComponents: View {
 struct BottomSheetView: View {
     var transperancy:Double = 0
     var cornerRadius:CGFloat = 0
+    var action:()->()
     @State var dismissFlag = false
     var body: some View {
-        ZStack(alignment:.bottom){
-            Color(.black)
-                .opacity(transperancy)
-                .edgesIgnoringSafeArea(dismissFlag ? .all : .top)
-                .onTapGesture {
-                    withAnimation {
-                        dismissFlag.toggle()
+        GeometryReader(content: { geometry in
+            ZStack(alignment:.bottom){
+                Color(.gray)
+                    .opacity(transperancy)
+                    .edgesIgnoringSafeArea(dismissFlag ? .all : .top)
+                    .onTapGesture {
+                        withAnimation {
+                            dismissFlag.toggle()
+                        }
                     }
+                VStack(alignment:.leading){
+                    VStack{
+                        AddYourComponents(action: action)
+                    }
+                    .frame(maxWidth:.infinity)
+                    .background(Color.white)
+                    .cornerRadius(cornerRadius, corners: [.topLeft,.topRight])
+                    .offset(y: dismissFlag ? geometry.size.height : 0)
                 }
-            VStack(alignment:.leading){
-                VStack{
-                    AddYourComponents()
-                }
-                .frame(maxWidth:.infinity)
-                .background(Color.white)
-                .cornerRadius(cornerRadius, corners: [.topLeft,.topRight])
-                .offset(y:dismissFlag ? UIScreen.main.bounds.height : 0)
             }
-        }
+        })
     }
 }
 
